3cee730094483ed4a2269bc860a5d8e751b3b322
fat-gril <15809244435@163.com>
2016-07-26 00:46:10 +0800

    move loadAllPromotions function to Promotion.all method

--- src/fixtures.js
+++
@@ -1,8 +0,0 @@
-const Promotion = require('./models/promotion');
-
-function loadPromotions() {
-
-  return [new Promotion('BUY_TWO_GET_ONE_FREE',['ITEM000000','ITEM000001','ITEM000005'])];
-}
-
-exports.loadPromotions = loadPromotions;
