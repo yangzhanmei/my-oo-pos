39f70dfa3416f12c39bd96c4991a2fe35c236b64
fat-gril <15809244435@163.com>
2016-07-26 01:05:19 +0800

    move buildReceipt function to Receipt.buildReceipt method

--- src/models/receipt.js
+++ src/models/receipt.js
@@ -6,6 +6,30 @@ class Receipt {
     this.total = total;
   }
 
+  buildReceiptText() {
+
+    const formatMoney = (money)=> {
+      return money.toFixed(2);
+    };
+
+    let receiptItemsText = this.receiptItems
+      .map(receiptItem => {
+        const cartItem = receiptItem.cartItem;
+        return `名称：${cartItem.item.name}，\
+数量：${cartItem.count}${cartItem.item.unit}，\
+单价：${formatMoney(cartItem.item.price)}(元)，\
+小计：${formatMoney(receiptItem.subtotal)}(元)`;
+      })
+      .join('\n');
+
+    return `***<没钱赚商店>收据***
+${receiptItemsText}
+----------------------
+总计：${formatMoney(this.total)}(元)
+节省：${formatMoney(this.savedTotal)}(元)
+**********************`;
+  }
+
   static buildReceipt(receiptItems) {
 
     let total = 0;
